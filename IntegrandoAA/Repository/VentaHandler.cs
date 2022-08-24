4e85f397e1b631378aac715ddd3c01cf95571388
JuanPablo-P <juanpablo.peramas@gmail.com>
2022-08-24 12:16:04 -0300

    1ra

--- IntegrandoAA/Repository/VentaHandler.cs
+++ IntegrandoAA/Repository/VentaHandler.cs
@@ -1,12 +1,36 @@
-﻿namespace IntegrandoAA.Repository
+﻿using IntegrandoAA.Model;
+using System.Data.SqlClient;
+
+namespace IntegrandoAA.Repository
 {
-    public class VentaHandler
+
+    public static class VentaHandler
     {
-        public static class VentaHandler
+        public const string ConnectionString = "Server=DESKTOP-8O5C5B1;" +
+            "Database=SistemaGestion;" +
+            "Trusted_Connection=True";
+        
+
+        public static List<Venta> GetVentas()
         {
-            public const string ConnectionString = "Server=DESKTOP-8O5C5B1;" +
-                "Database=SistemaGestion;" +
-                "Trusted_Connection=True";
+            List<Venta> resultados = new List<Venta>();
+            using (SqlConnection sqlConnection = new SqlConnection(ConnectionString))
+
+            {
+                var queryInstert = "INSERT into venta (Id, Comentarios) values (7,'algo'); select @@IDENTITY";
+                sqlConnection.Open();
+
+                double IdNuevo = 0;
+
+                using (SqlCommand comandoInsert = new SqlCommand(queryInstert, connection))
+                {
+                    IdNuevo =Convert.ToInt64(comandoInsert.ExecuteScalar());
+                }
+                connection.Close();
+                
+
+            }
+            return resultados;
         }
     }
 }
